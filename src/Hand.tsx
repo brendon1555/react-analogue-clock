6ced101f9f52aa4b5cda314348125aab7646addb
Brendon Lees <brendon1555@gmail.com>
2020-03-12 23:35:20 +1100

    fixes animation when passing 360 degrees

--- src/Hand.tsx
+++ src/Hand.tsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
+
 import { IHandColors } from './AnalogClock';
 import Box from './Box';
 
@@ -9,14 +10,15 @@ interface IHandTypeProps {
   handColors: IHandColors;
   handAngle: number;
   secondStartAngle: number;
+  showAnimation: boolean;
 }
 
 interface IHandTypeWithAngleProps {
-  type: string;
-  seconds: number;
-  minutes: number;
-  hours: number;
   handColors: IHandColors;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  type: string;
 }
 
 interface IFaceNumberProps {
@@ -36,76 +38,81 @@ const getHandAngle = ({ type, seconds, minutes, hours }: IHandAngleProps): numbe
   let handAngle = 0;
   switch (handType) {
     case 'second':
-      handAngle = seconds * 6;
+      handAngle = minutes * 360 + seconds * 6;
       break;
     case 'minute':
-      handAngle = minutes * 6;
+      handAngle = hours * 360 + minutes * 6;
       break;
     case 'hour':
       handAngle = hours * 30;
       break;
     default:
-      handAngle = 0;
+      handAngle = 360;
   }
   return handAngle;
 };
 
 const StyledHand = styled(Box)<IHandTypeProps>`
+  width: 50%;
+  right: 50%;
+  height: 3px;
   position: absolute;
-  right: 0;
-  bottom: 0;
-  transition: transform 0.3s cubic-bezier(.4,2.08,.55,.44);
+  top: 50%;
+  transform-origin: 100% 50%;
+  transform: rotate(${props => props.handAngle + 90}deg);
+  transition: ${props => (props.showAnimation ? 'transform 0.3s cubic-bezier(.4,2.08,.55,.44)' : 'none')};
   ${props =>
     props.type === 'second' &&
     css<IFaceNumberProps>`
+      height: 1%;
+      margin-top: -0.5%;
+      transform-origin: 80% 50%;
+      right: 40%;
       background: ${innerProps =>
         innerProps.handColors && innerProps.handColors.second ? innerProps.handColors.second : '#d81c7a'};
-      height: 50%;
-      left: 49.5%;
-      position: absolute;
-      top: 10%;
-      transform-origin: 50% 80%;
-      width: 1%;
-      z-index: 8;
-      transform: rotateZ(${innerProps => innerProps.handAngle}deg);
+      width: 50%;
+      z-index: 11;
     `}
   ${props =>
     props.type === 'minute' &&
     css<IFaceNumberProps>`
       background: ${innerProps =>
         innerProps.handColors && innerProps.handColors.minute ? innerProps.handColors.minute : '#d81c7a'};
-      height: 40%;
-      left: 49%;
-      position: absolute;
-      top: 10%;
-      transform-origin: 50% 100%;
-      width: 2%;
-      transform: rotateZ(${innerProps => innerProps.handAngle}deg);
+      width: 40%;
+      height: 2%;
+      margin-top: -1%;
+      z-index: 9;
     `}
   ${props =>
     props.type === 'hour' &&
     css<IFaceNumberProps>`
       background: ${innerProps =>
         innerProps.handColors && innerProps.handColors.hour ? innerProps.handColors.hour : '#d81c7a'};
-      height: 20%;
-      left: 48.75%;
-      position: absolute;
-      top: 30%;
-      transform-origin: 50% 100%;
-      width: 2.5%;
-      transform: rotateZ(${innerProps => innerProps.handAngle}deg);
+      width: 20%;
+      height: 2.5%;
+      margin-top: -1.25%;
+      z-index: 10;
     `}
 `;
 
 const Hand: React.FC<IHandTypeWithAngleProps> = ({ type, seconds, minutes, hours, handColors }) => {
   const [secondStartAngle] = useState(seconds * 6);
 
+  const [showAnimation, setShowAnimation] = useState(false);
+  const [handAngle, setHandAngle] = useState(getHandAngle({ type, seconds, minutes, hours }));
+
+  useLayoutEffect(() => {
+    setHandAngle(getHandAngle({ type, seconds, minutes, hours }));
+    setShowAnimation(true);
+  }, [type, seconds, minutes, hours]);
+
   return (
     <StyledHand
       type={type}
-      handAngle={getHandAngle({ type, seconds, minutes, hours })}
+      handAngle={handAngle}
       secondStartAngle={secondStartAngle}
       handColors={handColors}
+      showAnimation={showAnimation}
     />
   );
 };
