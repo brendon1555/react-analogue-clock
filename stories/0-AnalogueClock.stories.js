e6065fa83e86429158ba2141c25cb0795690d432
Brendon Lees <brendon1555@gmail.com>
2020-09-08 20:09:07 +1000

    adds story for multiple clocks

--- stories/0-AnalogueClock.stories.js
+++ stories/0-AnalogueClock.stories.js
@@ -26,6 +26,85 @@ export const CustomAnalogueClock = () => (
     size={number('size', 300)}
   />
 );
+export const FiveAnalogueClocks = () => (
+  <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+    <AnalogueClock
+      baseColor="#CEEC97"
+      borderColor="#F4B393"
+      borderWidth={5}
+      centerColor="#FC60A8"
+      handColors={{
+        hour: '#494368',
+        minute: '#494368',
+        second: '#494368',
+      }}
+      notchColor="#7A28CB"
+      numbersColor="#494368"
+      showNumbers={true}
+      size={250}
+    />
+    <AnalogueClock
+      baseColor="#DB2B39"
+      borderColor="#29335C"
+      borderWidth={5}
+      centerColor="#6B2034"
+      handColors={{
+        hour: '#000000',
+        minute: '#000000',
+        second: '#000000',
+      }}
+      notchColor="#F3A712"
+      numbersColor="#F0CEA0"
+      showNumbers={false}
+      size={250}
+    />
+    <AnalogueClock
+      baseColor="#2DBCE9"
+      borderColor="#9ACA3D"
+      borderWidth={5}
+      centerColor="#000022"
+      handColors={{
+        hour: '#000000',
+        minute: '#000000',
+        second: '#000000',
+      }}
+      notchColor="#001242"
+      numbersColor="#005E7C"
+      showNumbers={true}
+      size={250}
+    />
+    <AnalogueClock
+      baseColor="#3E000C"
+      borderColor="#FB4B4E"
+      borderWidth={0}
+      centerColor="#7C0B2B"
+      handColors={{
+        hour: '#ffffff',
+        minute: '#ffffff',
+        second: '#ffffff',
+      }}
+      notchColor="#D10000"
+      numbersColor="#FFCBDD"
+      showNumbers={true}
+      size={250}
+    />
+    <AnalogueClock
+      baseColor="#587B7F"
+      borderColor="#1E2019"
+      borderWidth={10}
+      centerColor="#394032"
+      handColors={{
+        hour: '#000000',
+        minute: '#000000',
+        second: '#000000',
+      }}
+      notchColor="#8DAB7F"
+      numbersColor="#CFEE9E"
+      showNumbers={true}
+      size={250}
+    />
+  </div>
+);
 
 DefaultAnalogueClock.story = {
   name: 'Default',
